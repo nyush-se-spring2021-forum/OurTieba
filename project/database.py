6fdb1ef4a8989a641c73c15dce38ee1c2199756c
JunchengDong0421 <jd4115@nyu.edu>
2021-03-19 15:21:00 +0800

    add model layer

--- project/database.py
+++ project/database.py
@@ -11,7 +11,6 @@ Base.query = db_session.query_property()
 
 
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
+    # import all models here (only the class name)
+    from .models import User
     Base.metadata.create_all(bind=engine)
