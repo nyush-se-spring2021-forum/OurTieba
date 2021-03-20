8ea399b496b4e97507a4c7a06ac8bd06377b1832
JunchengDong0421 <jd4115@nyu.edu>
2021-03-20 11:29:59 +0800

    add model layer

--- project/database.py
+++ project/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
