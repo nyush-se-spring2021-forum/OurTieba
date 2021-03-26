c6c05229a91771c5201e746b41e12eefbed20a23
JunchengDong0421 <jd4115@nyu.edu>
2021-03-26 14:02:57 +0800

    Views: add admin & api

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -1,7 +1,11 @@
 from ..views.guest import *
 from ..views.user import *
+from ..views.api import *
+from ..views.admin import *
 
 
 def register_blue(app):
     app.register_blueprint(user)
     app.register_blueprint(guest)
+    app.register_blueprint(api)
+    app.register_blueprint(admin)
---
+++ project/views/api.py
@@ -0,0 +1,3 @@
+from flask import Blueprint, render_template
+
+api = Blueprint("api", __name__, url_prefix="/api")
