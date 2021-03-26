4a0b6496ad0aefe7167a580c43af105f071b09c6
JunchengDong0421 <jd4115@nyu.edu>
2021-03-26 12:58:32 +0800

    Models: user & admin: add avatar Column

--- project/models/admin.py
+++ project/models/admin.py
@@ -12,13 +12,15 @@ class Admin(Base):
     Aid = Column(Integer, primary_key=True)
     password = Column(String(200))
     aname = Column(String(50), unique=True)
+    avatar = Column(String(100), default="")  # upon uploading, link=hash(Aid + timestamp) + ".png"
     timestamp = Column(DateTime, default=datetime.datetime.now().replace(microsecond=0))  # time of account creation
 
-    def __init__(self, password, aname=None, timestamp=None):
+    def __init__(self, password, aname=None, avatar=None, timestamp=None):
         if isinstance(timestamp, str):
             timestamp = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
         self.password = hashlib.sha3_512(password.encode()).hexdigest()
         self.aname = aname if aname else "admin" + str(self.Aid)
+        self.avatar = avatar
         self.timestamp = timestamp
 
     def __repr__(self):
--- project/models/user.py
+++ project/models/user.py
@@ -13,16 +13,18 @@ class User(Base):
     Uid = Column(Integer, primary_key=True)
     password = Column(String(200))
     uname = Column(String(50), unique=True)
+    avatar = Column(String(100), default="")  # upon uploading, link=hash(Uid + timestamp) + ".png"
     timestamp = Column(DateTime, default=datetime.datetime.now().replace(microsecond=0))  # time of account creation
 
     posts = relationship("Post", back_populates="owner")
     comments = relationship("Comment", back_populates="comment_by")
 
-    def __init__(self, password, uname, timestamp=None):
+    def __init__(self, password, uname, avatar=None, timestamp=None):
         if isinstance(timestamp, str):
             timestamp = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
         self.password = hashlib.sha3_512(password.encode()).hexdigest()
         self.uname = uname
+        self.avatar = avatar
         self.timestamp = timestamp
 
     def __repr__(self):
