d1af326bf8e950e95794f0637995f3c0db5ae0f4
JunchengDong0421 <jd4115@nyu.edu>
2021-03-27 18:09:32 +0800

    api: modify route "/post/add" add route "/like", "/dislike"

--- project/views/api.py
+++ project/views/api.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Blueprint, render_template, request, session, jsonify, redirect
 
 from ..database import *
@@ -8,19 +10,67 @@ api = Blueprint("api", __name__, url_prefix="/api")
 
 @api.route("/post/add", methods=["POST"])
 def create_post():
-    Bid = request.form.get("Bid")
-    if not Bid or not Bid.isnumeric():
-        return jsonify({"error": {"msg": "invalid data"}}), 404
-    data = {"Bid": Bid}
     Uid = session.get("Uid")
     if not Uid:
-        return render_template("create.html", data=data, error="Not logged in!")
+        return jsonify({"error": {"msg": "Not logged in!"}}), 403
+    Bid = request.form.get("Bid")
+    if not Bid or not Bid.isnumeric():
+        return jsonify({"error": {"msg": "invalid data"}}), 403
+    data = {"Bid": Bid}
     match_board = db_session.query(Board).filter(Board.Bid == Bid).all()
     if not match_board:
-        return jsonify({"error": {"msg": "invalid board ID"}}), 404
+        return jsonify({"error": {"msg": "invalid board ID"}}), 403
     title = request.form.get("title")
     content = request.form.get("content")
     new_post = Post(Uid, Bid, title, content)
     DB_session.add(new_post)
     DB_session.commit()
     return redirect("/board/" + Bid)
+
+
+@api.route("/like", methods=["POST"])
+def like():
+    Uid = session.get("Uid")
+    if not Uid:
+        return jsonify({"error": {"msg": "Not logged in!"}}), 403
+    target = request.form.get("target")
+    target_id = request.form.get("id")
+    action = request.form.get("like")
+    if target not in ["comment", "post"] or not target_id or not target_id.isnumeric() or action not in ["0", "1"]:
+        return jsonify({"error": {"msg": "invalid data"}}), 403
+    target_id = int(target_id)  # convert target_id to integer
+    query_from, filter_cond = (Comment, Comment.Cid == target_id) if target == "comment" else (
+    Post, Post.Pid == target_id)
+    match_target = db_session.query(query_from).filter(filter_cond).all()
+    if not match_target:
+        return jsonify({"error": {"msg": "invalid target ID"}}), 403
+    now = datetime.datetime.now()  # current timestamp
+    status = CommentStatus if target == "comment" else PostStatus
+    new_status = status(Uid, target_id, 0, 0, now) if action == "0" else status(Uid, target_id, 1, 0, now)
+    db_session.merge(new_status)
+    db_session.commit()
+    return jsonify({"success": 1}), 200
+
+
+@api.route("/dislike", methods=["POST"])
+def like():
+    Uid = session.get("Uid")
+    if not Uid:
+        return jsonify({"error": {"msg": "Not logged in!"}}), 403
+    target = request.form.get("target")
+    target_id = request.form.get("id")
+    action = request.form.get("like")
+    if target not in ["comment", "post"] or not target_id or not target_id.isnumeric() or action not in ["0", "1"]:
+        return jsonify({"error": {"msg": "invalid data"}}), 403
+    target_id = int(target_id)  # convert target_id to integer
+    query_from, filter_cond = (Comment, Comment.Cid == target_id) if target == "comment" else (
+    Post, Post.Pid == target_id)
+    match_target = db_session.query(query_from).filter(filter_cond).all()
+    if not match_target:
+        return jsonify({"error": {"msg": "invalid target ID"}}), 403
+    now = datetime.datetime.now()  # current timestamp
+    status = CommentStatus if target == "comment" else PostStatus
+    new_status = status(Uid, target_id, 0, 0, now) if action == "0" else status(Uid, target_id, 0, 1, now)
+    db_session.merge(new_status)
+    db_session.commit()
+    return jsonify({"success": 1}), 200
