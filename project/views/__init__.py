a5ffc59003be9b0da1feb39ed4fd12b6cfe4e41f
JunchengDong0421 <jd4115@nyu.edu>
2021-03-27 10:52:31 +0800

    Views update

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -5,7 +5,7 @@ from ..views.admin import *
 
 
 def register_blue(app):
-    app.register_blueprint(user)
+    app.register_blueprint(user_blue)
     app.register_blueprint(guest)
     app.register_blueprint(api)
     app.register_blueprint(admin)
--- project/views/user.py
+++ project/views/user.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, render_template
 
-user = Blueprint("user", __name__, url_prefix="/u")
+user_blue = Blueprint("user", __name__, url_prefix="/u")
 
 
-@user.route("/")  # http://localhost/u
+@user_blue.route("/")  # http://localhost/u
 def user_home():
     return "User Home"
 
 
-@user.route("/help")
+@user_blue.route("/help")
 def user_help():
     return render_template("help.html", type="user")
