23172bdf66e56671b4f62b85c6d343890e117c43
JunchengDong0421 <jd4115@nyu.edu>
2021-03-27 22:58:58 +0800

    views: add admin

--- project/views/admin.py
+++ project/views/admin.py
@@ -1,3 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template
 
 admin = Blueprint("admin", __name__, url_prefix="/admin")
+
+
+@admin.route("/")
+def admin_hello():
+    return redirect("/admin/dashboard")
+
+
+@admin.route("/dashboard")
+def admin_dashboard():
+    return render_template("admin_dashboard.html")
