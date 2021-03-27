001a33a5e195a06f75f920b551693aefc9b19598
JunchengDong0421 <jd4115@nyu.edu>
2021-03-27 10:12:58 +0800

    scrapper: optimize

--- project/scrapper.py
+++ project/scrapper.py
@@ -4,7 +4,7 @@ html_session = HTMLSession()
 
 
 def get_hot_news(**kwargs):
-    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=9d51c192354848748d129b08faea32ea"
+    url = "https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=9d51c192354848748d129b08faea32ea"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                              "Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.57"}
     res = html_session.get(url, headers=headers)
@@ -14,4 +14,5 @@ def get_hot_news(**kwargs):
 
 
 if __name__ == '__main__':
-    get_hot_news()
+    news = get_hot_news(num=3)
+    print(news)
