9f6b8e10d954487b02afb655466e53c4ea7281ea
JunchengDong0421 <jd4115@nyu.edu>
2021-03-31 00:41:53 +0800

    add login_required decorator to macros.py

--- project/configs/macros.py
+++ project/configs/macros.py
@@ -1,4 +1,20 @@
+import functools
+
+from flask import session, redirect
+
+
 # Macros
 PAGE_SIZE = 10
 RECOMMEND_NUM_BOARD = 10
 RECOMMEND_NUM_NEWS = 3
+
+
+def login_required(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        if session.get("Uid"):
+            return f(*args, **kwargs)
+        else:
+            return redirect("/login")
+
+    return wrapper
