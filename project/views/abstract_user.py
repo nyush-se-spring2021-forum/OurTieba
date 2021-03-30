3a078fd0d87992a6e3cde78ead3d6fce64eef805
JunchengDong0421 <jd4115@nyu.edu>
2021-03-30 12:07:15 +0800

    route "/": use scoped_session class instead of instance

--- project/views/abstract_user.py
+++ project/views/abstract_user.py
@@ -13,15 +13,15 @@ def index():
     hot_articles = get_hot_news(num=RECOMMEND_NUM_NEWS)
     hot_news = [{"title": a["title"], "abstract": a["description"], "link": a["url"],
                  "img_src": a["urlToImage"]} for a in hot_articles]
-    boards = db_session.query(Board).order_by(Board.hot.desc()).all()[:RECOMMEND_NUM_BOARD]
+    boards = Board.query.order_by(Board.hot.desc()).all()[:RECOMMEND_NUM_BOARD]
     recommend_boards = [{"Bid": b.Bid, "name": b.name, "hot": b.hot, "post_count": b.postCount} for b in boards]
     user_info = {}
     Uid = session.get("Uid")
     if Uid:
-        match_user = db_session.query(User).filter(User.Uid == Uid).first()
+        match_user = User.query.filter(User.Uid == Uid).first()
         user_info = {"Uid": Uid, "nickname": match_user.nickname, "avatar": match_user.avatar}
     data = {"boards": recommend_boards, "news": hot_news, "user_info": user_info}
-    db_session.commit()
+    DB_session.commit()
     return render_template("index.html", data=data)
 
 
