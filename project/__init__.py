0086d44cdaf2dff3236494ce64a3a94cdf83c6e8
Zihang-Xia <25659302+Zihang-Xia@users.noreply.github.com>
2021-04-01 16:11:12 +0800

    changed flask_bootstrap to native bootstrap

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, abort
-from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
 from .configs import *  # import configurations
@@ -15,7 +14,6 @@ def create_app():
     app = Flask(__name__, static_url_path="/")
     config_app(app)
     with app.app_context():
-        Bootstrap(app)
         Moment(app)
         init_db()
         register_blue(app)
