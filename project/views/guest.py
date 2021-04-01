1b7142e51ee1605c61e72a75777b5cc611ce4682
JunchengDong0421 <jd4115@nyu.edu>
2021-04-01 10:55:08 +0800

    guest: redirect to homepage if logged in

--- project/views/guest.py
+++ project/views/guest.py
@@ -1,13 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 
 guest = Blueprint("guest", __name__)
 
 
 @guest.route("/register")
 def register_interface():
+    if session.get("Uid"):
+        return redirect("/")
     return render_template("register.html")
 
 
 @guest.route("/login")
 def login_interface():
+    if session.get("Uid"):
+        return redirect("/")
     return render_template("login.html")
