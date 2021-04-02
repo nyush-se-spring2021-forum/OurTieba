409c9960d5b1d53a58bcce4fa83d62f3ce8a5bc3
JunchengDong0421 <jd4115@nyu.edu>
2021-04-02 11:32:13 +0800

    rename and add user_board_table(for subscribe function)

--- project/models/_tables.py
+++ project/models/_tables.py
@@ -6,3 +6,8 @@ user_report_table = Table("user_report", my_db.Base.metadata,
                           Column("Uid", Integer, ForeignKey("user.Uid")),
                           Column("Rid", Integer, ForeignKey("report.Rid"))
                           )
+
+user_board_table = Table("user_board", my_db.Base.metadata,
+                         Column("Uid", Integer, ForeignKey("user.Uid")),
+                         Column("Bid", Integer, ForeignKey("board.Bid"))
+                         )
