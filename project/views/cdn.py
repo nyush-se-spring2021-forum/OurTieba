f7ebe45a81e19b639641adfbea9c68c4d9e7c193
JunchengDong0421 <jd4115@nyu.edu>
2021-04-07 20:55:50 +0800

    route "/<filename>": add file existence check

--- project/views/cdn.py
+++ project/views/cdn.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, make_response, send_file
+import os
+
+from flask import Blueprint, make_response, send_file, abort
 
 
 cdn = Blueprint("cdn", __name__, url_prefix="/cdn")
@@ -6,6 +8,9 @@ cdn = Blueprint("cdn", __name__, url_prefix="/cdn")
 
 @cdn.route("/<filename>")
 def cdn_send_file(filename):
+    # check whether file exists
+    if not os.path.isfile(f"cdn/{filename}"):
+        abort(404)
     res = make_response(send_file(f"../cdn/{filename}"))
     res.headers["Cache-Control"] = "public, max-age=86400"
     res.headers.pop("Expires")
