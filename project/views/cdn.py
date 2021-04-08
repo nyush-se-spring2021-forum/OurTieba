e8ad199469872a972f54b45a56fcba598685870b
JunchengDong0421 <jd4115@nyu.edu>
2021-04-08 10:11:10 +0800

    modify cdn_send_file

--- project/views/cdn.py
+++ project/views/cdn.py
@@ -11,7 +11,7 @@ def cdn_send_file(filename):
     # check whether file exists
     if not os.path.isfile(CDN_PATH + filename):
         abort(404)
-    res = make_response(send_from_directory(CDN_PATH, filename))
+    res = make_response(send_from_directory("../" + CDN_PATH, filename))
     res.headers["Cache-Control"] = "public, max-age=86400"
     res.headers.pop("Expires")
     return res
