0f10c7272866f6aed3b020f5baa0f3b318685295
JunchengDong0421 <jd4115@nyu.edu>
2021-04-08 10:08:35 +0800

    modify cdn_send_file

--- project/views/cdn.py
+++ project/views/cdn.py
@@ -1,6 +1,6 @@
-import os
+from flask import Blueprint, make_response, abort, send_from_directory
 
-from flask import Blueprint, make_response, send_file, abort
+from ..configs import *
 
 
 cdn = Blueprint("cdn", __name__, url_prefix="/cdn")
@@ -9,9 +9,9 @@ cdn = Blueprint("cdn", __name__, url_prefix="/cdn")
 @cdn.route("/<filename>")
 def cdn_send_file(filename):
     # check whether file exists
-    if not os.path.isfile(f"cdn/{filename}"):
+    if not os.path.isfile(CDN_PATH + filename):
         abort(404)
-    res = make_response(send_file(f"../cdn/{filename}"))
+    res = make_response(send_from_directory(CDN_PATH, filename))
     res.headers["Cache-Control"] = "public, max-age=86400"
     res.headers.pop("Expires")
     return res
