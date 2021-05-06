a7702179b0534064e624b1d1aaca78a80eabd3d2
JunchengDong0421 <jd4115@nyu.edu>
2021-05-06 13:18:29 +0800

    use more dynamic upload api

--- project/static/ueditor/ueditor.config.js
+++ project/static/ueditor/ueditor.config.js
@@ -19,8 +19,9 @@
      * 因此，UEditor提供了针对不同页面的编辑器可单独配置的根路径，具体来说，在需要实例化编辑器的页面最顶部写上如下代码即可。当然，需要令此处的URL等于对应的配置。
      * window.UEDITOR_HOME_URL = "/xxxx/xxxx/";
      */
-    var URL = window.UEDITOR_HOME_URL || getUEBasePath();
-
+    var URL = window.UEDITOR_HOME_URL || getUEBasePath();  // http://127.0.0.1:5000/ueditor/
+	var host = window.location.host;  // 127.0.0.1:5000
+	var scheme = "http://"
     /**
      * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
      */
@@ -30,7 +31,7 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: "../../api/img/add"
+        , serverUrl: scheme + host + "/api/img/add"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
@@ -362,7 +363,7 @@
 		//output xss过滤
 		,outputXssFilter: true
 		// xss过滤白名单 名单来源: https://raw.githubusercontent.com/leizongmin/js-xss/master/lib/default.js
-		,whitList: {
+		,whiteList: {
 			a:      ['target', 'href', 'title', 'class', 'style'],
 			abbr:   ['title', 'class', 'style'],
 			address: ['class', 'style'],
