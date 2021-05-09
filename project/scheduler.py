63a1e058cb3a9ab4587b31a0d1e7c396181d891e
Ruhao Xin <rx434@nyu.edu>
2021-05-09 10:31:27 +0800

    update hot

--- project/scheduler.py
+++ project/scheduler.py
@@ -5,11 +5,39 @@ from .models import *
 
 # logger = init_logger()
 
+record = {}
 
 def update_hot():
     """ Not Finished!"""
+    global record
     print("updating hot...")
     # logger.logger.info("Start updating hot")
+    all_boards = my_db.query(Board, True)  #get all boards
+    #Firstly we need to check all Bid and delete non-existing Bid in record
+    Bid_list = []
+    for j in all_boards:
+        Bid_list.append(j.Bid)
+    for k in record.keys():
+        if k not in Bid_list:
+            record.pop(k)
+    #Then we start to do the updating:
+    for i in all_boards:
+        Bid = i.Bid
+        old_list = record.get(Bid, None)
+        if old_list is None:
+            old_list = [0, 0]
+            record[Bid] = old_list
+        #Algorithm of calculating a board's hot:
+        #50% * new_postCount + 50% * new_viewCount
+        new_postCount = i.postCount - old_list[0]
+        new_viewCount = i.viewCount - old_list[1]
+        new_hot = 0.5 * new_postCount + 0.5 * new_viewCount
+
+        #Then update the record dic
+        new_list = [i.postCount, i.viewCount]
+        record[Bid] = new_list
+        i.hot = new_hot
+
     print("average hot:", my_db.query(func.avg(Board.hot).label("average")).scalar())
     my_db.commit()
 
