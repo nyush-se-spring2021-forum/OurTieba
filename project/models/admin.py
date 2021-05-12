6ee77f4051e367a804ca1527c0a1e3b163c71ec0
JunchengDong0421 <jd4115@nyu.edu>
2021-05-12 22:29:18 +0800

    "default_avatar.jpg" -> "avatar/default_avatar.jpg"

--- project/models/admin.py
+++ project/models/admin.py
@@ -13,7 +13,7 @@ class Admin(my_db.Base):
     password = Column(String)
     aname = Column(String, unique=True)
     nickname = Column(String)
-    avatar = Column(String, default="default_avatar.jpg")  # upon uploading, link=hash(Aid + timestamp) + ".png"
+    avatar = Column(String, default="avatar/default_avatar.jpg")  # upon uploading, link=hash(Aid + timestamp) + ".png"
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)  # time of account creation
 
     def __init__(self, password, aname=None, nickname=None, avatar=None, timestamp=None):
