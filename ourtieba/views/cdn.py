11134c0a8c27036696aa9497d19440159b0f180c
JunchengDong0421 <jd4115@nyu.edu>
2021-05-13 18:14:03 +0800

    configure path correctly

--- ourtieba/views/cdn.py
+++ ourtieba/views/cdn.py
@@ -16,7 +16,7 @@ def cdn_send_file(filename):
     # check whether file exists
     if not os.path.isfile(CDN_ROOT_PATH + filename):
         abort(404)
-    res = make_response(send_file("../" + CDN_ROOT_PATH + filename))
+    res = make_response(send_file(os.getcwd() + "/" + CDN_ROOT_PATH + filename))
     res.headers["Cache-Control"] = "public, max-age=86400"
     res.headers.pop("Expires")
     return res
